b2db5448f74fd4e1eef163a41177b36d59e2d353
batalovM <b.maksim1708@gmail.com>
2024-03-09 13:56:31 +0700

    Initial commit

---
+++ modelLaba1/Approximations.cs
@@ -0,0 +1,49 @@
+﻿namespace modelLaba1;
+
+public class Approximations
+{
+    private int _size;
+    private int _numBorders;
+    public Approximations(int size, int numBorders)
+    {
+        _size = size;
+        _numBorders = numBorders;
+    }
+    public Tuple<double[], double[]> PiecewiseApproximationDensityFunction()
+    {
+        var hist = new double[_numBorders];
+        var bins = new double[_numBorders + 1];
+        var data = GenerateData();
+        Array.Sort(data);
+
+        for (var i = 0; i < _numBorders; i++)
+        {
+            var n = i + data[i];
+            var startIndex = i * data.Length / _numBorders;
+            var endIndex = Math.Min((i + 1) * data.Length / _numBorders, data.Length - 1);
+            bins[i] = data[startIndex];
+            //hist[i] = (double)(endIndex - startIndex) / data.Length / (data[endIndex] - data[startIndex]);
+            hist[i] = (-0.5) * n + 1;
+        }
+
+        bins[_numBorders] = data[^1];
+        var binCenters = new double[_numBorders];
+    
+        for (var i = 0; i < _numBorders; i++)
+        {
+            binCenters[i] = (bins[i] + bins[i + 1]) / 2;
+        }
+
+        return Tuple.Create(binCenters, hist);
+    }
+    private double[] GenerateData()
+    {
+        var rand = new Random();
+        var data = new double[_size];
+        for (var i = 0; i < _size; i++)
+        {
+            data[i] = rand.NextDouble();
+        }
+        return data;
+    }
+}
---
+++ modelLaba1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using modelLaba1;
+
+class Program
+{
+    static void Main()
+    {
+        var solution = new Approximations(100, 4);
+        var result = solution.PiecewiseApproximationDensityFunction();
+        // Вывод результатов
+        Console.WriteLine("Границы:");
+        foreach (var center in result.Item1)
+        {
+            Console.Write($"{center:F3} ");
+        }
+        
+        Console.WriteLine("\nЗначения плотности:");
+        foreach (var density in result.Item2)
+        {
+            Console.Write($"{density:F2} ");
+        }
+    }
+}

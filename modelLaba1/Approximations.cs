185639da7e10e8fa1a0610ec8a287b154cfbc4a5
batalovM <b.maksim1708@gmail.com>
2024-03-10 00:47:14 +0700

    upt

--- modelLaba1/Approximations.cs
+++ modelLaba1/Approximations.cs
@@ -4,46 +4,88 @@ public class Approximations
 {
     private int _size;
     private int _numBorders;
-    public Approximations(int size, int numBorders)
+    private double[] _intervals = { 0, 0.268, 0.586, 1, 2 };
+    public Approximations(int size)
     {
         _size = size;
-        _numBorders = numBorders;
     }
-    public Tuple<double[], double[]> PiecewiseApproximationDensityFunction()
+
+    public void Modeling(List<double> data)
     {
-        var hist = new double[_numBorders];
-        var bins = new double[_numBorders + 1];
-        var data = GenerateData();
-        Array.Sort(data);
-
-        for (var i = 0; i < _numBorders; i++)
-        {
-            var n = i + data[i];
-            var startIndex = i * data.Length / _numBorders;
-            var endIndex = Math.Min((i + 1) * data.Length / _numBorders, data.Length - 1);
-            bins[i] = data[startIndex];
-            //hist[i] = (double)(endIndex - startIndex) / data.Length / (data[endIndex] - data[startIndex]);
-            hist[i] = (-0.5) * n + 1;
-        }
-
-        bins[_numBorders] = data[^1];
-        var binCenters = new double[_numBorders];
-    
-        for (var i = 0; i < _numBorders; i++)
-        {
-            binCenters[i] = (bins[i] + bins[i + 1]) / 2;
-        }
-
-        return Tuple.Create(binCenters, hist);
-    }
-    private double[] GenerateData()
-    {
-        var rand = new Random();
-        var data = new double[_size];
         for (var i = 0; i < _size; i++)
         {
-            data[i] = rand.NextDouble();
+            var randomXiPlus1 = GenerateRandomNumber();
+            var y = GenerateY(randomXiPlus1);
+            data.Add(y);
+            Console.Write($"y: {y:F3} ");
         }
-        return data;
+        KolmogorovTest(data);
+    }
+    private double GenerateRandomNumber()
+    {
+        var random = new Random();
+        return random.NextDouble();
+    }
+    private int SelectInterval(double randomNumber)
+    {
+        for (var i = 0; i < _intervals.Length - 1; i++)
+        {
+            if (randomNumber >= _intervals[i] && randomNumber < _intervals[i + 1])
+            {
+                return i;
+            }
+        }
+        return -1; // В случае ошибки
+    }
+    private double ScaleRandomNumber(double randomNumber, int intervalIndex)
+    {
+        return (randomNumber - _intervals[intervalIndex]) / (_intervals[intervalIndex + 1] - _intervals[intervalIndex]);
+    }
+    private double GenerateY(double randomNumber)
+    {
+        var intervalIndex = SelectInterval(randomNumber);
+        var scaledRandomNumber = ScaleRandomNumber(randomNumber, intervalIndex);
+        var y = _intervals[intervalIndex] + (_intervals[intervalIndex + 1] - _intervals[intervalIndex]) * scaledRandomNumber;
+        return y;
+    }
+    private double TheoreticalCdf(double x)
+    {
+        if (x < 0)
+        {
+            return 0;
+        }
+        if (x is >= 0 and < 1)
+        {
+            return x;
+        }
+        return 1;
+    }
+    private double EmpiricalCdf(IReadOnlyCollection<double> data, double x)
+    {
+        var count = data.Count(d => d <= x);
+        return (double)count / data.Count;
+    }
+    private void KolmogorovTest(IReadOnlyCollection<double> data)
+    {
+        var cdfValues = data.Select(val => EmpiricalCdf(data, val)).ToArray();
+        var theoreticalCdfValues = data.Select(TheoreticalCdf).ToArray();
+        double D = 0;
+        for (var i = 0; i < data.Count; i++)
+        {
+            var diff1 = Math.Abs(cdfValues[i] - theoreticalCdfValues[i]);
+            var diff2 = Math.Abs(theoreticalCdfValues[i] - cdfValues[i]);
+
+            D = Math.Max(D, Math.Max(diff1, diff2));
+        }
+
+        const double alpha = 0.05; // Уровень значимости
+        var criticalValue = Math.Sqrt(-0.5 * Math.Log(alpha / 2)) / Math.Sqrt(data.Count);
+
+        Console.WriteLine($"\nСтатистика: {D:F3} ");
+        Console.WriteLine($"Критическое значение: {criticalValue:F5}");
+
+        Console.WriteLine(D <= criticalValue
+            ? "Гипотеза о согласии с законом распределения не отвергается"
+            : "Гипотеза о согласии с законом распределения отвергается");
     }
 }
--- modelLaba1/Program.cs
+++ modelLaba1/Program.cs
@@ -1,23 +1,14 @@
 ﻿
+
 using modelLaba1;
 
 class Program
 {
     static void Main()
     {
-        var solution = new Approximations(100, 4);
-        var result = solution.PiecewiseApproximationDensityFunction();
-        // Вывод результатов
-        Console.WriteLine("Границы:");
-        foreach (var center in result.Item1)
-        {
-            Console.Write($"{center:F3} ");
-        }
-        
-        Console.WriteLine("\nЗначения плотности:");
-        foreach (var density in result.Item2)
-        {
-            Console.Write($"{density:F2} ");
-        }
+        var data = new List<double>();
+
+        var test = new Approximations(100);
+        test.Modeling(data);
     }
 }
